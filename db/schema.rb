4a2bce943bac8b410652e7894acfd85a0c06b136
今吉恒介 <ikkorokoro@icloud.com>
2021-05-11 23:32:29 +0900

    データベース null制約 追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_02_140206) do
+ActiveRecord::Schema.define(version: 2021_05_11_140255) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
