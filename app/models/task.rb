e99bf85775d9b8c9f671ba3bbf5bf292d9f2ff05
今吉恒介 <ikkorokoro@icloud.com>
2021-05-12 23:40:43 +0900

    文字数軽減 独自validetes 追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: {maximum: 30}
+  #独自メソッドの場合は複数形のsは不要
+  validate :validate_name_not_including_conma
+
+  private
+
+  def validate_name_not_including_conma
+    errors.add(:name, 'カンマを含めることはできません') if name&.include?(',')
+  end
 end
