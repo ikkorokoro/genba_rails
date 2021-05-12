3c30cff4676b2a6f35c048929d3aa9f3b4f64bb2
今吉恒介 <ikkorokoro@icloud.com>
2021-05-12 23:52:12 +0900

    before_validation 追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
   validates :name, presence: true, length: {maximum: 30}
   #独自メソッドの場合は複数形のsは不要
   validate :validate_name_not_including_conma
@@ -7,5 +8,9 @@ class Task < ApplicationRecord
 
   def validate_name_not_including_conma
     errors.add(:name, 'カンマを含めることはできません') if name&.include?(',')
+  end
+
+  def set_nameless_name
+    self.name = '名前なし' if name.blank?
   end
 end
