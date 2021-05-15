fdc5075367fe50594b6c58c08fc6e95b24283a05
今吉恒介 <ikkorokoro@icloud.com>
2021-05-15 23:14:48 +0900

    user_controller コード追記

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,19 @@
 class Admin::UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
-  def edit
+  def create 
+    @user = User.new(user_parmas)
+    if @user.save
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}を登録しました」"
+    else
+      render :new
+    end
   end
 
-  def show
-  end
-
-  def index
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
