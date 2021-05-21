e6a8cea1643d7180f8c6d4a0095476d87a78b68a
今吉恒介 <ikkorokoro@icloud.com>
2021-05-21 22:04:30 +0900

    sessions_controller createアクション　追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,31 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: session_parmas[:email])
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'ログインしました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+
 end
+=begin 
+* authenticateメソッド
+has_secure_passwordを記述した時に、自動で追加された認証のメソッド
+引数のpasswordをハッシュ化して,データベースに保存されてあるdigestと一致するか調べる。
+* find_byメソッド
+ユーザーデータが見つからない場合、変数userはnilとなるので、条件分岐のauthenticateメソッドの処理の前でぼっち演算子を用いて
+エラーにならないように回避する
+* proivatメソッド
+アクションではないユーティリティメソッドはprivateメソッドに定義しておくことを推奨
+=end
