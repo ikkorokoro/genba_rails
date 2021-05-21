0f58182261b02acc4b39519a41f2e32f79564d8f
今吉恒介 <ikkorokoro@icloud.com>
2021-05-21 22:05:07 +0900

    helper_method :current_user 追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  private
+  def current_user
+    @current_user || = User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
+=begin 
+* current_user
+session[:user_id]が存在していればそのままcurrent_userを使用できる。
+session[:user_id]がnilの場合はfind_byでデータベースから探し出し、current_userに代入する 
+=end
