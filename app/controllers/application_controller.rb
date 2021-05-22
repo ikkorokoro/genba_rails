f725d0aea756c40d8f3f54cf7fb64ddb24dabcff
今吉恒介 <ikkorokoro@icloud.com>
2021-05-22 22:08:55 +0900

    ユーザーがログインしているかどうか確認する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,20 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  before_action :login_required
 
   private
   def current_user
-    @current_user || = User.find_by(id: session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def login_required
+    redirect_to login_url unless current_user
   end
 end
 =begin 
 * current_user
 session[:user_id]が存在していればそのままcurrent_userを使用できる。
 session[:user_id]がnilの場合はfind_byでデータベースから探し出し、current_userに代入する 
+* login_required
+ログインしているかどうかチェックする
 =end
