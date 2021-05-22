591419ed6c4112e307fed44e60c3d809220ec3c6
今吉恒介 <ikkorokoro@icloud.com>
2021-05-22 22:08:23 +0900

    ログアウト アクション追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,22 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required
   def new
   end
 
   def create
-    user = User.find_by(email: session_parmas[:email])
+    user = User.find_by(email: session_params[:email])
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: 'ログインしました'
     else
       render :new
     end
+  end
+
+  def destroy
+    #sessionのsuer_idのデータのみ削除したい場合、session.delete(:user_id)と記述すればよい
+    reset_session
+    redirect_to root_url, notice: 'ログアウトしました'
   end
 
   private
