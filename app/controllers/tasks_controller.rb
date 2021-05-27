20058e4834847af94e0372578495737c803991b9
今吉恒介 <ikkorokoro@icloud.com>
2021-05-27 19:26:10 +0900

    ログインユーザーからデータを取得し、セキュリティ強化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,14 @@
 class TasksController < ApplicationController
+=begin 
+  もし悪意あるユーザーがURLに他のユーザーが作った、taskのid入れて,もしそのデータが存在していれば
+  簡単に閲覧、編集、削除することができてしまうのでcurrent_user.task.findし記述してデータを探すようにする
+=end
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   def show
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -12,20 +16,20 @@ class TasksController < ApplicationController
   end
 
   def create 
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
     if @task.save
-      redirect_to @task, notice: "タスク「#{task.name}」を登録しました"
+      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました"
     else
       render :new
     end
   end
 
   def edit
-    @task = Task.find(params[:id])
+    @task =current_user.tasks.find(params[:id])
   end
 
   def update
-  task = Task.find(params[:id])
+  task = current_user.tasks.find(params[:id])
   task.update!(task_params)
   redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました"
   end
