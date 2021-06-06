a4edef2deda8e13f96c08a22dda8a20cade92fa5
今吉恒介 <ikkorokoro@icloud.com>
2021-06-06 23:48:14 +0900

    specが失敗する時

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   before_validation :set_nameless_name
-  validates :name, presence: true, length: {maximum: 30}
+  # validates :name, presence: true, length: {maximum: 30}
   #独自メソッドの場合は複数形のsは不要
   validate :validate_name_not_including_conma
   belongs_to :user
