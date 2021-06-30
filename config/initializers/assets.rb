0d47e867b452fbca1e1ed2b35386ebd154c010a7
今吉恒介 <ikkorokoro@icloud.com>
2021-06-30 17:36:11 +0900

    assets.rb アセット関連の設定

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,13 +2,18 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+#アセットのバージョンを文字列で設定する記述
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
+
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+#アセットの探索パスはデフォルトではnode_modulesのディレクトリが指定されていないため上記の記述により指定する
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
+
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+#上記の記述はプレコンパイルするファイルを配列で指定します。デフォルトでassete以下のjs、cssはプレコンパイル対象になっている
